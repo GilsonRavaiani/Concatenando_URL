6aa5291b741f83d3e6ce26ae211d838c42e1fee5
Guiando <Guiando@G036>
2022-05-24 17:45:06 -0300

    Add project files.

---
+++ concatenacao/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace concatenacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+                var musica1 = "https://www.youtube.com/";
+                var musica2 = "watch?v=vM0QTlrTpUA&list=RDEMZugo";
+                var musica3 = "S1nmaQoa_ecvfpwxUQ&start_radio=1&rv=n_LcVqqHSY8";
+
+
+            Console.WriteLine(musica1+musica2+musica3);
+            Console.ReadLine();
+        }
+    }
+}
